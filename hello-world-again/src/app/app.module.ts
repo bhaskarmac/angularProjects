63495245a7292d22e0c6c3f3620a855170fe7bfa
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2018-05-01 15:26:49 +0530

    Adding example of two way data binding

--- hello-world-again/src/app/app.module.ts
+++ hello-world-again/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -18,7 +18,8 @@ import { SuccessComponent } from './success/success.component';
     SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hello-world-again/src/app/servers/servers.component.ts
+++ hello-world-again/src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(()=> {
@@ -20,11 +20,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-
+    this.serverCreationStatus = 'Server was created and name is ' + this.serverName;
   }
 
   onUpdateServername(event: Event) {
-    this.serverName = <HTMLInputElement>event.target.value;
+    this.serverName = (<HTMLInputElement>event.target).value;
     console.log(event.target.value);
   }
 
